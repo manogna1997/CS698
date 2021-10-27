80a928ca7cbeed411532c615d697d7fb7c53d550
Abhilash Keerthi <abhilashkeerthi@Abhilashs-MacBook-Pro.local>
2021-10-27 07:52:55 -0700

    adding memeory cal script

---
+++ install.sh
@@ -0,0 +1,34 @@
+pip3 install pycryptodome
+pip3 install pandas       
+pip3 install scikit-learn
+
+1  pip --version
+   24   apt-get update
+    2  cd /opt/mywork/
+    3  ls
+    4  python memory_calculation.py 
+    5  pip install pycrypto
+    6  python memory_calculation.py 
+    7  pip install pycryptodome
+    8  python memory_calculation.py 
+    9  pip install numpy
+   10  python memory_calculation.py 
+   11  pip install sklearn
+   12  pip install scipy
+   13  pip install numpy
+
+   18  apt-get install python-virtualenv python-pip
+   19  apt-get build-dep python-numpy python-scipy
+   20  apt-get install libblas-dev
+   21  apt-get install python-scipy
+   29  apt-get build-dep python-numpy python-scipy
+   30  apt-get install python-scipy
+   32  pip install scikit-learn
+   33  apt-get install gfortran libopenblas-dev liblapack-dev
+   36  apt-get install python-numpy python-scipy
+   41  apt-get install python-scipy
+   43  pip install -v scipy
+   45  pip install sklearn
+   51  pip install pandas
+   52  python memory_calculation.py 
+   53  history
---
+++ memory_calculation.py
@@ -0,0 +1,142 @@
+from Crypto.Cipher import Salsa20
+import hashlib
+from base64 import b64encode
+from Crypto.Cipher import ChaCha20
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from math import sqrt,log10
+import numpy as np
+import struct
+import codecs
+import sys
+import pandas as pd
+Threshold= 0.4;
+
+plaintext = b'be with failur you with failure success will follows'
+key= b't'
+
+print("Plain key:\t",plaintext)
+print("Secret key:\t",key)
+
+secret = hashlib.sha256()
+
+secret.update(key)
+
+print("Key used:\t",b64encode(secret.digest()))
+
+cipher = Salsa20.new(key=secret.digest())
+enc=cipher.encrypt(plaintext)
+
+
+ct = b64encode(enc).decode('utf-8')
+nonce = b64encode(cipher.nonce).decode('utf-8')
+print("\n---Salsa20 Encrypt")
+print(" Nonce:",nonce)
+print(" Cipher:",ct)
+def salm():
+    
+    memorys1=ct
+    memorys= sys.getsizeof(memorys1)
+    print("memory used by salsa : " + str(memorys))
+    mins=min(memorys1)
+    mins1=max(memorys1)
+    treshs= mins+mins1
+    ress = ''.join(format(ord(i), '08b') for i in treshs)
+    salsath=int(ress,2)/2
+    print("threshold value of salsa is : ",salsath)
+
+    
+
+
+print("\n---Salsa20 Decrypt")
+cipher = Salsa20.new(key=secret.digest(), nonce=cipher.nonce)
+plainoutstext = cipher.decrypt(enc)
+print(" Decrypted:\t",plainoutstext)
+
+
+
+cipher = ChaCha20.new(key=secret.digest())
+ciphertext = cipher.encrypt(plaintext)
+
+nonce = b64encode(cipher.nonce).decode('utf-8')
+ct = b64encode(ciphertext).decode('utf-8')
+print("\n---ChaCha20 Encrypt")
+print(" Nonce:",nonce)
+print(" Cipher:",ct)
+def cham():
+    
+    memoryc1=ct
+    memoryc= sys.getsizeof(memoryc1)
+    print("the memory used by chacha: " + str(memoryc))
+    minc=min(memoryc1)
+    minc1=max(memoryc1)
+##    print("min value of chacha is :", minc)
+##    print("max value of chacha is :", minc1)
+    tresh= minc+minc1
+    #print("threshold of chacha is: ", tresh)
+    resc = ''.join(format(ord(i), '08b') for i in tresh)
+    chachamom=int(resc,2)/2
+    print("threshold vlaue of chacha is : ",chachamom)
+
+print("\n---ChaCha20 Decrypt")
+cipher = ChaCha20.new(key=secret.digest(), nonce=cipher.nonce)
+plainoutctext= cipher.decrypt(ciphertext)
+print(" Decrypted:\t",plainoutctext)
+
+def sal():
+    byteData1 = plainoutstext
+    s1= codecs.decode(byteData1, 'UTF-8')
+    print(s1)
+
+    # initializing string
+    test_str1 = s1
+
+    # printing original string
+    #print("The original string is : " + str(test_str1))
+
+    # using join() + ord() + format()
+    # Converting String to binary
+    res1 = ''.join(format(ord(i), '08b') for i in test_str1)
+
+    # printing result
+    #print("The string after binary conversion : " + str(res))
+    b1=0.4*int(res1,2)
+    print("through out put for salsa: ",b1)
+    
+    
+def cha():
+        
+    byteData = plainoutctext
+    s= codecs.decode(byteData, 'UTF-8')
+    print(s)
+
+    # initializing string
+    test_str = s
+
+    # printing original string
+    #print("The original string is : " + str(test_str))
+
+    # using join() + ord() + format()
+    # Converting String to binary
+    res = ''.join(format(ord(i), '08b') for i in test_str)
+
+    # printing result
+    #print("The string after binary conversion : " + str(res))
+    b=0.4*int(res,2)
+    print("through out put for chacha: ",b)
+##def data():
+##    data = dict([(t, np.ones(shape=5000, dtype=int).astype(t))
+##             for t in plainoutctext])
+##    df = pd.DataFrame(data)
+##    df.head()
+    
+sal()
+cha()
+salm()
+cham()
+##data()
+
+
+
+##c=np.mean((plainoutctext)*(Threshold));
+##print("c21: ", c);
